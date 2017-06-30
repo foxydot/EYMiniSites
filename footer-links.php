de2251502a16158a02162dd17e05254713e4ada9
CMO <catherine@msdlab.com>
2017-06-30 11:40:10 -0400

    some footer style

--- footer-links.php
+++ footer-links.php
@@ -1,20 +1,20 @@
 <div class="row">
-    <div class="animate footer-links col-md-4">
-        <div class="block">
+    <div class="animate footer-links col-md-3">
+        <div class="footer-link-block">
             <a href="#" class="footer-link">
                 <img src="./img/link-transform-1.jpg" />
             </a>
         </div>
     </div>
-    <div class="animate footer-links col-md-4">
-        <div class="block">
+    <div class="animate footer-links col-md-5">
+        <div class="footer-link-block">
             <a href="#" class="footer-link">
                 <img src="./img/link-video-1.jpg" />
             </a>
         </div>
     </div>
     <div class="animate footer-links col-md-4">
-        <div class="block">
+        <div class="footer-link-block">
             <a href="#" class="footer-link">
                 <img src="./img/link-ebook-1.jpg" />
             </a>
