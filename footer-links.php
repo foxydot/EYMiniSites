a1060212fa64b867248869dc74c98a4c195e1c51
CMO <catherine@msdlab.com>
2017-06-30 14:36:29 -0400

    footer titles

--- footer-links.php
+++ footer-links.php
@@ -1,9 +1,13 @@
 <div class="row">
+    <div class="animate footer-links col-md-12">
+        <h3>More from the EY Digital Insurance Innovation Center</h3>
+    </div>
     <div class="animate footer-links col-md-3">
         <div class="footer-link-block">
             <a href="#" class="footer-link">
-                <img src="./img/link-transform-1.jpg" />
+                <img src="./img/link-white-paper-1.jpg" />
             </a>
+            <h4>White Paper</h4>
         </div>
     </div>
     <div class="animate footer-links col-md-5">
@@ -11,6 +15,7 @@
             <a href="#" class="footer-link">
                 <img src="./img/link-video-1.jpg" />
             </a>
+            <h4>Video</h4>
         </div>
     </div>
     <div class="animate footer-links col-md-4">
@@ -18,6 +23,7 @@
             <a href="#" class="footer-link">
                 <img src="./img/link-ebook-1.jpg" />
             </a>
+            <h4>EBook</h4>
         </div>
     </div>
 </div>
