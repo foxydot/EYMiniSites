9bfabd5409003b6127c2adaff21a1a08eacf20fc
CMO <catherine@msdlab.com>
2017-07-19 17:44:06 -0400

    "polish" footer links

--- footer-links.php
+++ footer-links.php
@@ -2,28 +2,31 @@
     <div class="animate footer-links col-md-12">
         <h3>More from the EY Digital Insurance Innovation Center</h3>
     </div>
-    <div class="animate footer-links col-md-3">
-        <div class="footer-link-block">
+    <div class="animate footer-links col-md-4">
+        <div class="footer-link-block whitepaper">
+            <h4>White paper</h4>
+            <h5>Find out how to drive immediate value and enable future innovation</h5>
             <a href="#" class="footer-link">
                 <img src="./img/link-white-paper-1.jpg" />
             </a>
-            <h4>White Paper</h4>
-        </div>
-    </div>
-    <div class="animate footer-links col-md-5">
-        <div class="footer-link-block">
-            <a href="#" class="footer-link">
-                <img src="./img/link-video-1.jpg" />
-            </a>
-            <h4>Video</h4>
         </div>
     </div>
     <div class="animate footer-links col-md-4">
-        <div class="footer-link-block">
+        <div class="footer-link-block video">
+            <h4>Video</h4>
+            <h5>See how digital makes the possible a reality in insurance</h5>
+            <a href="#" class="footer-link">
+                <img src="./img/link-video-1.jpg" />
+            </a>
+        </div>
+    </div>
+    <div class="animate footer-links col-md-4">
+        <div class="footer-link-block ebook">
+            <h4>eBook</h4>
+            <h5>Review the drivers, enablers and technology that are defining the future</h5>
             <a href="#" class="footer-link">
                 <img src="./img/link-ebook-1.jpg" />
             </a>
-            <h4>EBook</h4>
         </div>
     </div>
 </div>
